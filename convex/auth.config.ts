7ee577324d1d2478781ce629cbc58367231b2433
chamikaNimnajith <148580813+chamikaNimnajith@users.noreply.github.com>
2025-03-10 07:36:32 +0530

    1

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://enjoyed-kingfish-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ src/providers/convex-client-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {ConvexReactClient} from "convex/react";
+import {ClerkProvider, useAuth} from "@clerk/nextjs";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {ReactNode} from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+// whithout ! typesrite think that value could be undefined. by putting a ! we tell typescript that it always there
+
+export function ConvexClientProvider({children}: {children: ReactNode}) {
+    return (
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+}
